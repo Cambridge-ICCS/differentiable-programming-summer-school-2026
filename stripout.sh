f2039f8cfb03e21655cfd6069f36c386eeb391c2
Joe Wallwork <jw2423@cam.ac.uk>
2026-04-21 10:40:53 +0100

    Add spaces between arguments

--- stripout.sh
+++ stripout.sh
@@ -11,13 +11,13 @@ fi
 # NOTE: the metadata.kernelspec is kept since quarto needs it to start
 # the kernel and execute the cells when rendering
 nbstripout --extra-keys="metadata.celltoolbar " \
-  "metadata.language_info.codemirror_mode.version" \
-  "metadata.language_info.pygments_lexer" \
-  "metadata.language_info.version metadata.toc" \
-  "metadata.notify_time metadata.varInspector" \
-  "cell.metadata.heading_collapsed" \
-  "cell.metadata.hidden" \
-  "cell.metadata.code_folding" \
-  "cell.metadata.tags" \
-  "cell.metadata.init_cell" \
+  "metadata.language_info.codemirror_mode.version " \
+  "metadata.language_info.pygments_lexer " \
+  "metadata.language_info.version metadata.toc " \
+  "metadata.notify_time metadata.varInspector " \
+  "cell.metadata.heading_collapsed " \
+  "cell.metadata.hidden " \
+  "cell.metadata.code_folding " \
+  "cell.metadata.tags " \
+  "cell.metadata.init_cell " \
   session1.ipynb session2.ipynb
